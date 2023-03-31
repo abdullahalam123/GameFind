f765e7f90982254d35451903a90c1e1a2328c2f2
abdullahalam123 <abdullah.alam.tapadar2000@gmail.com>
2023-03-31 19:27:13 +0530

    Build Search Component

--- game-found/src/components/ColorSwitch.tsx
+++ game-found/src/components/ColorSwitch.tsx
@@ -10,7 +10,7 @@ const ColorSwitch = () => {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text>Dark Mode</Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 };
--- game-found/src/components/NavBar.tsx
+++ game-found/src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/react.svg";
 import ColorSwitch from "./ColorSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
+      <SearchInput />
       <ColorSwitch />
     </HStack>
   );
